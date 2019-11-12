d4f386ec899239c3795e21a22039499d3612f9f0
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-12 14:45:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,13 @@ class Customer {
 }
 
 class Meal {
-  
+  constructor(title, price) {
+    this.id = ++mealId;
+    this.title = title;
+    this.price = price;
+    
+    store.meals.push(this);
+  }
 }
 
 class Delivery {
